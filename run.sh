3078ae7882ebff7d34bcde29c0463623b03c3063
Soichi Hayashi <hayashis@iu.edu>
2019-08-08 10:45:31 -0400

    Update run.sh
    re-adding enableOutputOverwrite

--- run.sh
+++ run.sh
@@ -160,9 +160,9 @@ mrconvert mask.mif -stride 1,2,3,4 ./mask/mask.nii.gz -force -nthreads $NCORE
     -stepSize $(jq -r .stepsize config.json) \
     -writeColors \
     -verboseLevel 0 \
+    -enableOutputOverwrite \
     -output output.vtk
 
-#-enableOutputOverwrite \
 
 # convert output vtk to tck
 tckconvert output.vtk track/track.tck -force -nthreads $NCORE
